006d0c56f394bbfaa1e544d1492b44ab1eed3ca8
Hut146 <hut4712@gmail.com>
2025-10-03 22:48:16 -0400

    Arreglando App.jsx

--- React/Frontend/FarmaciaOasis/src/App.jsx
+++ React/Frontend/FarmaciaOasis/src/App.jsx
@@ -1,28 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './modules/navbar/navbar';
-import Dashboard from './modules/dashboard/dashboard';
+import { AppShell } from '@mantine/core';
+import Layout from './modules/navbar/navbar';
+import Dashboard from './modules/dashboard/Dashboard';
 import Inventario from './modules/inventario-ventas/inventario';
 import HistorialVentas from './modules/historial-ventas/historial-ventas';
 import IngresosEgresos from './modules/ingresos-egresos/ingresos-egresos';
 
-import { AppShell } from '@mantine/core';
-import Layout from './modules/navbar/Navbar';
-import Dashboard from './modules/dashboard/Dashboard';
-import Inventario from './modules/inventario-ventas/Inventario';
-import HistorialVentas from './modules/historial-ventas/Historial-ventas';
-import IngresosEgresos from './modules/ingresos-egresos/Ingresos-Egresos';
-
 function App() {
   return (
     <Router>
-      <Navbar>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/inventario" element={<Inventario />} />
-          <Route path="/historial-ventas" element={<HistorialVentas />} />
-          <Route path="/ingresos-egresos" element={<IngresosEgresos />} />
-        </Routes>
-      </Navbar>
       <AppShell header={{ height: 'max-height' }}>
         <Layout>
           <Routes>
